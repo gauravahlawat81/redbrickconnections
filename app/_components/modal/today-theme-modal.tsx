c02f6931b20c6ee5e2448cb64ced267f7c17ca4c
Gaurav Ahlawat <gauravahlawat81@gmail.com>
2025-01-25 13:04:56 +0530

    Added game for 25th Jan

--- app/_components/modal/today-theme-modal.tsx
+++ app/_components/modal/today-theme-modal.tsx
@@ -19,37 +19,37 @@ export default function TodayThemeModal({ isOpen, onClose }: TodayThemeModalProp
 
         {/* Everything that was originally inside your second modal */}
         <div className="text-black text-base md:text-lg mb-4 w-full">
-          <h3 className="font-semibold mb-2">Ahmedabad Businesses</h3>
+          <h3 className="font-semibold mb-2">___ Play</h3>
           <ul className="list-disc list-inside ml-4">
-            <li>Rasna, Vadilal, Gandhi Soda Shop, Adani.</li>
+            <li>Foreplay, Screenplay, Downplay, Coldplay</li>
             
           </ul>
         </div>
 
         <div className="text-black text-base md:text-lg mb-4 w-full">
-          <h3 className="font-semibold mb-2">Eateries on Campus</h3>
+          <h3 className="font-semibold mb-2">Terms Related to the Placement Process</h3>
           <ul className="list-disc list-inside ml-4">
-            <li>Prithvi, Kamla, Radhika, Jaya Bairav Nath: Food spots at IIMA. Prithvi Mess, Kamla Café. Radhika’s and JBN (Jaya Vairab Nath)</li>
+            <li>Relate, Dream, Hold, Accept</li>
           </ul>
         </div>
 
         <div className="text-black text-base md:text-lg mb-4 w-full">
-          <h3 className="font-semibold mb-2">___ Sarabhai</h3>
+          <h3 className="font-semibold mb-2">Regional groups at IIMA</h3>
           <ul className="list-disc list-inside ml-4">
-            <li>Vikram: Renowned scientist and founder of ISRO</li>
-            <li>Mallika: Activist, Padma Bhushan awardee, and accomplished Kuchipudi and Bharatanatyam dancer who uses art for social change.</li>
-            <li>Mrinalini: Celebrated Bharatanatyam dancer and founder of Darpana Academy.</li>
-            <li>Anasuya: Social worker, women&apos;s rights activist, and nationalist leader.</li>
+            <li>WTF - WIMWIAN Tamil Fraternity</li>
+            <li>SPAM: Some People Are Mallus</li>
+            <li>USA: United States of Andhra</li>
+            <li>Token Diversity Gang</li>
           </ul>
         </div>
 
         <div className="text-black text-base md:text-lg mb-4 w-full">
-          <h3 className="font-semibold mb-2">Names with Body Parts in Them</h3>
+          <h3 className="font-semibold mb-2">4th</h3>
           <ul className="list-disc list-inside ml-4">
-            <li>Chinmay:  like Prof. Chinmay Tumbe - contains &quot;Chin.&quot;</li>
-            <li>Palmo - like Thupstan Palmo - contains &quot;Palm.&quot;</li>
-            <li>Armando - contains &quot;Arm.&quot;</li>
-            <li>Handa: Contains &quot;Hand.&quot;</li>
+            <li>IIMA @ Sangharsh: IIMA finished 4th this year.</li>
+            <li>Alan Bean: The 4th person to walk on the moon.</li>
+            <li>Press: Known as the 4th Estate in democracy.</li>
+            <li>US Independence: Celebrated on July 4th.</li>
           </ul>
         </div>
 
--- app/_examples.ts
+++ app/_examples.ts
@@ -2,26 +2,26 @@ import { Category } from "./_types";
 
 export const categories: Category[] = [
   {
-    category: "Ahmedabad Businesses",
-    items: ["Rasna", "Vadilal", "Gandhi Soda Shop", "Adani"],
+    category: "___ PLAY",
+    items: ["Fore", "Screen", "Down", "cold"],
     level: 1,
   },
   {
-    category: "____ Sarabhai",
-    items: ["Vikram", "Mallika", "Mrinalini", "Anasuya"],
+    category: "Terms Related to the Placement Process",
+    items: ["Relate", "Dream", "Hold", "Accept"],
     level: 2,
   },
   {
-    category: 'Eateries on campus',
-    items: ["Prithvi", "Kamla", "Radhika", "Jaya Bairav Nath"],
+    category: 'Regional groups at IIMA',
+    items: ["SPAM", "USA", "Token Diversity Gang", "WTF"],
     level: 3,
    
   },
   {
-    category: "Names with body parts in them",
-    items: ["Chinmay", "Palmo", "Armando", "Handa"],
+    category: "4th",
+    items: ["IIMA @ Sangharsh", "Alan Bean", "Press", "US Independence"],
     level: 4,
   },
 ];
 
-export const gameID: number = 6
+export const gameID: number = 7
